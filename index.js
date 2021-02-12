1dcd8db4ccc038f7347c001a365385abeb96d34c
Leigh Murray <leighmurray@gmail.com>
2021-02-12 15:10:00 +0100

    moved to node

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.use('/', express.static('public/'));
+
+io.on('connection', (socket) => {
+
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
