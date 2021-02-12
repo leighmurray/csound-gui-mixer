7df6b7d7c9e7efad2b9d2585f7fe89cc44c01d18
Leigh Murray <leighmurray@gmail.com>
2021-02-12 17:29:35 +0100

    added paper and audio loops

--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,7 @@ function moduleDidLoad() {
     SetCutoffEnabled();
 
     attachListeners();
+    loadTrack();
     window.addEventListener("unload", function(e) {
         if (csound != null)
         csound.destroy();
@@ -141,12 +142,24 @@ function togglePlay() {
     }
 }
 
+function loadTrack() {
+    if (!loaded) {
+        console.log("loading file...");
+        csound.CopyUrlToLocal("/audio/track01.wav", "track01.wav", function() {
+            loaded = true;
+            console.log("Ready to play. \n");
+        });
+    } else {
+        csound.UpdateStatus("to load a new file, first refresh page!")
+    }
+}
+
 function handleFileSelect(evt) {
     if (!loaded) {
         var files = evt.target.files;
         var f = files[0];
         var objectURL = window.URL.createObjectURL(f);
-        csound.CopyUrlToLocal(objectURL, "audiofile.wav", function() {
+        csound.CopyUrlToLocal(objectURL, "track01.wav", function() {
             loaded = true;
             console.log("Ready to play. \n");
         });
